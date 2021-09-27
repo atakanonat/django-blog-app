2db1196b4cb1dc8f6e7ee12d568878ab59677b0c
atakanonat <atakanonat628@gmail.com>
2021-09-27 12:18:39 +0300

    profile image sizes optimized

--- user/models.py
+++ user/models.py
@@ -2,16 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from PIL import Image
 
 # Create your models here.
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    account_img = models.FileField(unique=True,
-                                   default="user-01.png", verbose_name="accountImg")
+    account_img = models.ImageField(unique=True, upload_to='userImg/',
+                                    default="/userImg/user-01.png", verbose_name="accountImg")
     userAbout = models.CharField(
         blank=True, max_length=200, verbose_name="userAbout")
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.account_img.path)
+        img.thumbnail((560, 280))
+        img.save(self.account_img.path)
 
 
 @receiver(post_save, sender=User)
